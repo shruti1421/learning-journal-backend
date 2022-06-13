45628fb0b99436752c9d4f43aafc3712b1601c09
Smriti Naik <smritinaik1421@gmail.com>
2022-06-13 11:02:28 +0530

    login-register--proxy error

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser=require('body-parser');
 const dotenv= require("dotenv");
 const connectDB = require('./config/db');
+var cors = require('cors');
 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    credentials: true,
+  }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,5 +25,7 @@ app.use('/api/journals', require('./routes/journals'));
 app.use('/api/journals/share', require('./routes/shareJournal'));
 
 const PORT = process.env.PORT || 5000;
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
